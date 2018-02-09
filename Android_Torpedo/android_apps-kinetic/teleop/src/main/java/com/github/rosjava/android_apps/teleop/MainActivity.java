cc559c2449da9f34233e39b4ca9013fc6ad942fb
= <Isaiah.Erb@gmail.com>
2018-02-08 22:08:09 -0500

    added LinAlg library, testing publishing

--- Android_Torpedo/android_apps-kinetic/teleop/src/main/java/com/github/rosjava/android_apps/teleop/MainActivity.java
+++ Android_Torpedo/android_apps-kinetic/teleop/src/main/java/com/github/rosjava/android_apps/teleop/MainActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2013 OSRF.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 package com.github.rosjava.android_apps.teleop;
 
 import android.os.Bundle;
@@ -33,11 +17,7 @@ import org.ros.node.NodeMainExecutor;
 
 import java.io.IOException;
 
-/**
- * @author murase@jsk.imi.i.u-tokyo.ac.jp (Kazuto Murase)
- */
 public class MainActivity extends RosAppActivity {
-	private RosImageView<sensor_msgs.CompressedImage> cameraView;
 	private VirtualJoystickView virtualJoystickView;
 	private Button backButton;
 
@@ -54,9 +34,6 @@ public class MainActivity extends RosAppActivity {
 		setMainWindowResource(R.layout.main);
 		super.onCreate(savedInstanceState);
 
-        cameraView = (RosImageView<sensor_msgs.CompressedImage>) findViewById(R.id.image);
-        cameraView.setMessageType(sensor_msgs.CompressedImage._TYPE);
-        cameraView.setMessageToBitmapCallable(new BitmapFromCompressedImage());
         virtualJoystickView = (VirtualJoystickView) findViewById(R.id.virtual_joystick);
         backButton = (Button) findViewById(R.id.back_button);
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -80,23 +57,22 @@ public class MainActivity extends RosAppActivity {
                     NodeConfiguration.newPublic(local_network_address.getHostAddress(), getMasterUri());
 
         String joyTopic = remaps.get(getString(R.string.joystick_topic));
-        String camTopic = remaps.get(getString(R.string.camera_topic));
 
         NameResolver appNameSpace = getMasterNameSpace();
         joyTopic = appNameSpace.resolve(joyTopic).toString();
-        camTopic = appNameSpace.resolve(camTopic).toString();
 
-		cameraView.setTopicName(camTopic);
         virtualJoystickView.setTopicName(joyTopic);
-		
-		nodeMainExecutor.execute(cameraView, nodeConfiguration
-				.setNodeName("android/camera_view"));
+
 		nodeMainExecutor.execute(virtualJoystickView,
 				nodeConfiguration.setNodeName("android/virtual_joystick"));
-        } catch (IOException e) {
-            // Socket problem
-        }
 
+
+		    System.out.println("Socket works");
+
+        } catch (IOException e) {
+           System.out.println("Socket error: " + e.getMessage());
+
+        }
 	}
 	
 	  @Override
