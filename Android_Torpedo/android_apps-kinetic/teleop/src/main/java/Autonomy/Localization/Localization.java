04ce79b1a0787805e8ee3c7574385135288b38c5
Michael Skupien <meskupie@gmail.com>
2018-03-14 15:46:55 -0400

    moved corespondence matching

--- Android_Torpedo/android_apps-kinetic/teleop/src/main/java/Autonomy/Localization/Localization.java
+++ Android_Torpedo/android_apps-kinetic/teleop/src/main/java/Autonomy/Localization/Localization.java
@@ -6,6 +6,8 @@ import org.ros.rosjava_geometry.Quaternion;
 import org.ros.rosjava_geometry.Transform;
 import org.ros.rosjava_geometry.Vector3;
 
+import java.util.ArrayList;
+
 import Autonomy.MyQuaternion;
 import geometry_msgs.Twist;
 
@@ -55,16 +57,21 @@ public class Localization {
     private SimpleMatrix data_thrust;
     private Quaternion data_imu;
     private double data_depth;
+
+    // Particle Filter
+    ParticleCloud particles;
+    private SimpleMatrix param_A_mat = new SimpleMatrix(SIZE_STATES, SIZE_STATES);
+    private SimpleMatrix param_B_mat = new SimpleMatrix(SIZE_STATES, SIZE_INPUTS);
+
+    // Data fitting
+    private MapTarget[] param_map;
     private CameraTarget[] data_camera_targets_front;
     private CameraTarget[] data_camera_targets_rear;
     private CameraTarget[] data_camera_targets_all;
+    Vector3[] fitting_points_target;
+    Vector3[] fitting_points_map;
 
-    private SimpleMatrix param_A_mat = new SimpleMatrix(SIZE_STATES, SIZE_STATES);
-    private SimpleMatrix param_B_mat = new SimpleMatrix(SIZE_STATES, SIZE_INPUTS);
-    private MapTarget[] param_map;
 
-    // Data fitting
-    TargetProcessing target_processor;
 
     public Localization() {}
 
@@ -92,6 +99,46 @@ public class Localization {
         return true;
     }
 
+    public void matchCorrespondences(Transform pose){
+        ArrayList<Vector3> points_target_list = new ArrayList<>();
+        ArrayList<Vector3> points_map_list = new ArrayList<>();
+        for(int i = 0; i < data_camera_targets_all.length; i++){
+            double best_score = 0.5; // Score threshold that has to be beet
+            // Best correspondence
+            boolean found_option = false;
+            Vector3 best_map = new Vector3(0,0,0);
+            Vector3 best_target = new Vector3(0,0,0);
+            // Calculate a geometry related to the camera target
+            Transform target_inertial_trans = pose.multiply(data_camera_targets_all[i].getTargetTransform());
+            Quaternion target_inertial_quat = target_inertial_trans.getRotationAndScale();
+            Vector3 target_inertial_vect = target_inertial_trans.getTranslation();
+            Transform unit_vector_trans = new Transform(new Vector3(1,0,0),new Quaternion(0,0,0,1));
+            // Calculate a unit direction vector of the inertial camera laser
+            Vector3 target_unit_vect = new Transform(new Vector3(0,0,0),target_inertial_quat).multiply(unit_vector_trans).getTranslation();
+            for(int j = 0; j < param_map.length; j++){
+                if(data_camera_targets_all[i].getTargetId() == param_map[j].getTargetId()){ // colours are the same
+                    Vector3 map_test_point = param_map[j].getPoint().subtract(target_inertial_vect);
+                    double scale = map_test_point.dotProduct(target_unit_vect)/target_unit_vect.dotProduct(target_unit_vect);
+                    if(scale > 0){
+                        Vector3 camera_test_point = target_unit_vect.scale(scale);
+                        double score = camera_test_point.subtract(map_test_point).getMagnitudeSquared();
+                        if(score < best_score){
+                            best_score = score;
+                            found_option = true;
+                            best_map = map_test_point;
+                            best_target = camera_test_point;
+                        }
+                    }
+                }
+            }
+            if(found_option){
+                points_map_list.add(best_map);
+                points_target_list.add(best_target);
+            }
+        }
+        fitting_points_map = points_map_list.toArray(new Vector3[0]);
+        fitting_points_target = points_target_list.toArray(new Vector3[0]);
+    }
 
 
     public boolean setInitialOrientation() {
@@ -164,7 +211,6 @@ public class Localization {
     public boolean setMapData(MapTarget[] _data_map_targets) {
         param_map = _data_map_targets;
         ready_map = true;
-        target_processor = new TargetProcessing(param_map);
         ready_localization = myIsReady();
         return ready_map;
     }
