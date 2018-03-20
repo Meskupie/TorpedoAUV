39fae9ae2236ab6510dd4a5a387ab4dbb093f424
Michael Skupien <meskupie@gmail.com>
2018-03-19 22:34:29 -0400

    completed UI merge, builds

--- Android_Torpedo/android_apps-kinetic/teleop/src/main/java/com/github/rosjava/android_apps/teleop/MainActivity.java
+++ Android_Torpedo/android_apps-kinetic/teleop/src/main/java/com/github/rosjava/android_apps/teleop/MainActivity.java
@@ -326,18 +326,29 @@ import Communication.USBDeviceWrapper;
 			}
 		};
 
-
 		private USBDeviceWrapper initializeSerial(final USBDeviceWrapper device_wrapper) {
+			final String ACTION_USB_ATTACHED = "android.hardware.usb.action.USB_DEVICE_ATTACHED";
+			final String ACTION_USB_DETACHED = "android.hardware.usb.action.USB_DEVICE_DETACHED";
+			final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
 			this.usb_manager = (UsbManager) getSystemService(Context.USB_SERVICE);
 			// TODO: change to serial number
 			// Attempt to find device by vendor and product id
 			HashMap<String, UsbDevice> deviceList = this.usb_manager.getDeviceList();
-			for (String key : deviceList.keySet()) {
+			for (String key: deviceList.keySet()){
 				UsbDevice device = deviceList.get(key);
-				if (device.getVendorId() == device_wrapper.VENDOR_ID && device.getProductId() == device_wrapper.PRODUCT_ID) {
-					device_wrapper.usbDevice = device;
-					Log.d(TAG_LOG, "USB device " + device_wrapper.name + " found with serial number " + device.getSerialNumber());
-					break;
+				if(device.getVendorId() == device_wrapper.VENDOR_ID && device.getProductId() == device_wrapper.PRODUCT_ID) {
+					boolean seen = false;
+					int id = device.getDeviceId();
+					for(int i = 0; i < (usb_number_registered); i++){
+						if(id == usb_ids[i]){seen = true;}
+					}
+					if(!seen){
+						usb_ids[usb_number_registered] = id;
+						usb_number_registered++;
+						device_wrapper.usbDevice = device;
+						Log.d(TAG_LOG, "USB device " + device_wrapper.name + " found with id: " + id);
+						break;
+					}
 				}
 			}
 			// setup broadcast receiver for user permission granting
@@ -346,44 +357,43 @@ import Communication.USBDeviceWrapper;
 					String action = intent.getAction();
 					if (ACTION_USB_PERMISSION.equals(action)) {
 						synchronized (this) {
-							device_wrapper.usbConnection = usb_manager.openDevice(device_wrapper.usbDevice);
+							device_wrapper.usbConnection = usb_manager.openDevice(device_wrapper.usbDevice );
 							device_wrapper.serial = UsbSerialDevice.createUsbSerialDevice(device_wrapper.usbDevice, device_wrapper.usbConnection);
 							if (device_wrapper.serial != null) {
 								if (device_wrapper.serial.open()) { //Set Serial Connection Parameters.
-									//serial.setDTR(true);
+									//device_wrapper.serial.setDTR(true);
 									device_wrapper.serial.setBaudRate(115200);
 									device_wrapper.serial.setDataBits(UsbSerialInterface.DATA_BITS_8);
 									device_wrapper.serial.setStopBits(UsbSerialInterface.STOP_BITS_1);
-									//serial.setStopBits(UsbSerialInterface.STOP_BITS_1);
 									device_wrapper.serial.setParity(UsbSerialInterface.PARITY_NONE);
 									device_wrapper.serial.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
 
 									device_wrapper.serial.read(device_wrapper.callback);
 								} else {
-									Log.d(TAG_ERROR, "USB serial port not open for " + device_wrapper.name);
+									Log.d(TAG_ERROR,"USB serial port not open for "+device_wrapper.name);
 								}
 							} else {
-								Log.d(TAG_ERROR, "USB serial Port is null for " + device_wrapper.name);
+								Log.d(TAG_ERROR,"USB serial Port is null for "+device_wrapper.name);
 							}
 							if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
-								if (device_wrapper.usbDevice != null) {
+								if(device_wrapper.usbDevice != null){
 									//call method to set up device communication
 								}
 							} else {
-								Log.d(TAG_ERROR, "USB Permission denied for " + device_wrapper.name);
+								Log.d(TAG_ERROR,"USB Permission denied for "+device_wrapper.name);
 							}
 						}
 					}
 				}
 			};
 			// request permission from the user on create
-			if (device_wrapper.usbDevice != null) {
+			if(device_wrapper.usbDevice != null) {
 				PendingIntent mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
 				IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
 				this.registerReceiver(device_wrapper.usbReceiver, filter);
-				this.usb_manager.requestPermission(device_wrapper.usbDevice, mPermissionIntent);
+				this.usb_manager.requestPermission(device_wrapper.usbDevice,mPermissionIntent);
 			} else {
-				Log.d(TAG_ERROR, "USB could not find " + device_wrapper.name + " on startup");
+				Log.d(TAG_ERROR,"USB could not find "+device_wrapper.name+" on startup");
 			}
 			return device_wrapper;
 		}
@@ -490,7 +500,6 @@ import Communication.USBDeviceWrapper;
                 }
             }, 200);
 
-
 		}
 
         private void onWaitingForLock() {
@@ -556,14 +565,6 @@ import Communication.USBDeviceWrapper;
 //                }
 //            }, 5000);
 		}
-
-//        backButton = (Button) findViewById(R.id.back_button);
-//        backButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                onBackPressed();
-//            }
-//        });
 
 		private void onReadyToLaunch(){
             header.setText(R.string.ready_to_launch);
@@ -661,15 +662,5 @@ import Communication.USBDeviceWrapper;
             button_nav_middle.setOnTouchListener(null);
             button_nav_bottom.setOnTouchListener(null);
         }
-
-		public void onClickButton1(View v) {
-		}
-
-		public void onClickButton2(View v) {
-		}
-
-		public void onClickButton3(View v) {
-			new SerialWrite().execute(new Object[]{usb_device_smc.serial, message_manager.msg_smc_sensors.getRequest()});
-		}
 
 	}
